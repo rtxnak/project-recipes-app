24f08f30ee243be22f73df2e52094305d69a24f6
Gregory <gregorysak90@gmail.com>
2022-01-28 19:09:11 -0300

    Requisito 13, implementa radios button no search

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../button/Button';
 import Input from '../input/Input';
 
 function SearchBar() {
@@ -11,6 +12,40 @@ function SearchBar() {
         type="text"
         testid="search-input"
         value="searchBar"
+      />
+      <label htmlFor="ingredients">
+        Ingredient
+        <input
+          data-testid="ingredient-search-radio"
+          type="radio"
+          value="ingredients"
+          name="firstLetter"
+        />
+      </label>
+      <label htmlFor="name">
+        Name
+        <input
+          data-testid="name-search-radio"
+          type="radio"
+          value="name"
+          name="firstLetter"
+        />
+      </label>
+      <label htmlFor="firstLetter">
+        First Letter
+        <input
+          data-testid="first-letter-search-radio"
+          type="radio"
+          value="firstLetter"
+          name="firstLetter"
+        />
+      </label>
+
+      <Button
+        label="Search"
+        testid="exec-search-btn"
+        // onClick
+        // disabled=""
       />
     </div>
   );
