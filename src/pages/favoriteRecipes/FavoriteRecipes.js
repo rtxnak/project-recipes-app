eb7687932dd2c94caaaf129cffe4f8c3169b8d78
Gregory <gregorysak90@gmail.com>
2022-02-08 11:32:45 -0300

    Implementando list de receitas favoritas

--- src/pages/favoriteRecipes/FavoriteRecipes.js
+++ src/pages/favoriteRecipes/FavoriteRecipes.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Header from '../../components/header/Header';
 
 function FavoriteRecipes() {
+  // const [favRecipes, setFavRecipes] = useState([]);
+  const getFavoriteRecipe = () => {
+    const recipeFavorites = localStorage.getItem('favoriteRecipes');
+    const arrFavoriteRecipes = JSON.parse(recipeFavorites);
+    // setFavRecipes(arrFavoriteRecipes);
+    console.log(arrFavoriteRecipes);
+    return arrFavoriteRecipes;
+  };
   return (
     <div>
       <Header
         label="Favorite Recipes"
         testid="page-title"
       />
+      { getFavoriteRecipe().map((recipe, i) => (
+        <li key={ i }>{recipe.name}</li>
+      ))}
     </div>
   );
 }
